de67e92e2c72e925a055997aa616ccee6123f5ac
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-05-07 16:30:26 +0200

    inserito header

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    /* array dati pasta */
     $data = [
         [
             "src" => "https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg",
@@ -136,9 +137,11 @@ Route::get('/', function () {
             "descrizione" => "Altro elemento cult della famiglia de lo Spaghetto Quadrato (N.1 Spaghetto Quadrato. Una new entry che sarà molto apprezzata sia dai consumatori che dagli chef, perché il Ditale Quadrato è un formato deliziosamente piccolo ma sostanzioso.<br>A dispetto del nome che fa pensare ad una pastina è un formato di pasta assolutamente versatile, adatto a moltissime ricette di primi piatti.<br>La sua consistenza soda si sprigiona in bocca con un\'esplosione di emozioni, grazie agli spessori corposi, al colore elegantemente ambrato, alla texture delicatamente ruvida, cangiante e piacevolissima al tatto che trattiene il condimento sulla superficie.<br>Il Ditale Quadrato sembra ideale per preparazioni strutturate come la ricetta con crema di broccoletto siciliano, calamari e pomodori semi secchi profumata al limone e carbone d\'olive nere."
         ]
     ];
+    /* inizializzo 3 array vuoti in cui suddividere i tipi di pasta */
     $lunga = [];
     $corta = [];
     $cortissima = [];
+    /* divido i tipi di pasta nei 3 array */
     foreach($data as $pasta){
         if($pasta['tipo'] == 'lunga'){
             $lunga[] = $pasta;
@@ -151,6 +154,8 @@ Route::get('/', function () {
         }
     }    
     return view('home',[
-        'pasta' => $data
+        'lunga' => $lunga,
+        'corta' => $corta,
+        'cortissima' => $cortissima,
     ]);
 });
